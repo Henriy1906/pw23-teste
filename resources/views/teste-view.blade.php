20fd65afce501654c18981cd61b3e4bf2d6629cb
Henriy1906 <henriquedeitosdias@gmail.com>
2023-05-25 09:15:30 -0300

    Initial commit

---
+++ resources/views/teste-view.blade.php
@@ -0,0 +1,9 @@
+@extends('includes.base')
+
+@section('title', 'Título legal')
+
+@section('content')
+<p>Este é o meu primeiro arquivo Blade</p>
+<p>Estou emocioneido 😢</p>
+<p>Minha variável que veio de longe: {{ $valor_da_controller }}</p>
+@endsection
