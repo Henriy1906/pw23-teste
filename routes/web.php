86fb515d115140c8294b682e2ce6fb10c209bce6
Henriy1906 <henriquedeitosdias@gmail.com>
2023-06-29 10:12:31 -0300

    usuarios

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,5 @@ Route::post('/produtos/edit/{produto}', [ProdutosController::class, 'editSave'])
 Route::get('/produtos/delete/{produto}', [ProdutosController::class, 'delete'])->name('produtos.delete');
 
 Route::delete('/produtos/delete/{produto}', [ProdutosController::class, 'deleteForReal'])->name('produtos.deleteForReal');
+
+Route::get('/usuarios', [UsuariosController::class, 'index'])->name('usuarios');
